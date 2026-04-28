9230aebb905a04cd00217af4230c120f92411084
Mozzie <mozzie009@gmail.com>
2026-04-28 15:45:18 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,16 @@
 let iframe;
 let player;
 var render_scale = 10;
+let video_width;
+let video_height;
+const video_ratio = 1.777;
 
 function setup() {
   iframe = document.getElementById("vimeo-player");
   player = new Vimeo.Player(iframe);
   load_times();
   create_ui();
+  scale_elements();
 }
 
 function draw() {}
@@ -53,4 +57,21 @@ function to_seconds(string) {
   time += s2[1] * 60;
   time += s2[2] * 1;
   return time;
+}
+
+function scale_elements() {
+  video_width = displayWidth; // video width
+  video_height = video_width / video_ratio;
+
+  iframe.style.width = video_width + "px";
+  iframe.style.height = video_height + "px";
+
+  sel.position(20, video_height + 10);
+
+  ui_jump_scene.position(sel.size().width + 20, video_height + 10);
+  inp.position(20, video_height + 40);
+  ui_jump_time.position(sel.size().width + 20, video_height + 40);
+  inp.size(sel.size().width - 7, 20);
+  //sel.size().width
+  // sel.size(20,40)
 }
