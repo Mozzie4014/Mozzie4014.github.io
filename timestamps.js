d0c648b9f02b2b2608ff74925fe0936b898af8dc
Mozzie <mozzie009@gmail.com>
2026-04-28 13:58:37 -0600

    Create timestamps.js

---
+++ timestamps.js
@@ -0,0 +1,79 @@
+function load_times() {
+  times = [];
+
+  times.push({
+    name: "Sleeping Beauty",
+    id: "header",
+    tags: [],
+
+    time: "0:0:0",
+  });
+
+  times.push({
+    name: "Variation 1",
+    id: "variation1",
+    tags: [],
+    time: "0:8:15",
+  });
+
+  times.push({
+    name: "Variation 2",
+    id: "variation2",
+    tags: [],
+    time: "0:9:43",
+  });
+
+  times.push({
+    name: "Fairy Coda Prologue",
+    id: "",
+    tags: [],
+    time: "0:15:0",
+  });
+  times.push({
+    name: "Fairy Coda End Prologue",
+    id: "",
+    tags: [],
+    time: "0:17:0",
+  });
+
+  times.push({
+    name: "Caraboss Entrance Prologue",
+    id: "",
+    tags: [],
+    time: "0:18:6",
+  });
+  times.push({
+    name: "Bat Prologue Dance End",
+    id: "",
+    tags: [],
+    time: "0:22:50",
+  });
+
+  times.push({
+    name: "Caraboss Exit Prologue",
+    id: "",
+    tags: [],
+    time: "0:24:31",
+  });
+
+  times.push({
+    name: "Act1",
+    id: "",
+    tags: [],
+    time: "0:25:55",
+  });
+
+  times.push({
+    name: "Knitters Dance 1 Act 1",
+    id: "",
+    tags: [],
+    time: "0:25:55",
+  });
+
+  times.push({
+    name: "Cat & Puss in boots",
+    id: "",
+    tags: [],
+    time: "1:15:10",
+  });
+}
